b4f578200b14cd4c612119d63dcb5641ad1129fd
victoria-huang <vctrah@gmail.com>
2018-04-16 15:53:29 -0400

    add basic controller methods to users, loans, items, and welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   resources :users
-  resources :loans
+  resources :loans, only: [:index, :show, :new, :create, :edit, :update]
   resources :items
 end
