c7f143a5e48664c60ea2bf0bb01fc77014383b46
J.D. Bean <jonathandbean@gmail.com>
2018-04-16 16:59:36 -0400

    name and email are required fields for user. email must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
   has_many :borrowed_items, through: :loans, source: :item
   # has_many :shared_items, foreign_key: "lender_id", class_name: "Item"
   has_many :items
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
 end
