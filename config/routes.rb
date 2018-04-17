d9a302d26e115af8dd41339e8d51d705960bf64d
J.D. Bean <jonathandbean@gmail.com>
2018-04-17 12:19:09 -0400

    corrected user sign-in redirect flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'auth'
-  root 'welcome#index'
+
+  root to: 'welcome#index'
+  # user_root_path to: 'users#index'
   # root to: "home#index"
 
   resources :users
   resources :loans, only: [:index, :show, :new, :create, :edit, :update]
   resources :items
 
+  # user_root to: 'users#index'
 
 end
