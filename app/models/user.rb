7141b0ccd6e758f08c42c52dacb33e0c75995c9b
J.D. Bean <jonathandbean@gmail.com>
2018-04-18 09:59:56 -0400

    switching phone format to xxx.xxx.xxxx

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,8 +51,8 @@ class User < ApplicationRecord
                       allow_blank: true
 
   validates_format_of :phone,
-                      :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-                      :message => "- Phone numbers must be in xxx-xxx-xxxx format.",
+                      :with => /\(?[0-9]{3}\)?\.[0-9]{3}\.[0-9]{4}/,
+                      :message => "- Phone numbers must be in xxx.xxx.xxxx format.",
                       allow_blank: true
 
 
