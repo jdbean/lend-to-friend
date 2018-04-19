feb47ed691d7b88cfc2e12bc9eee7a1b6ce1b632
J.D. Bean <jonathandbean@gmail.com>
2018-04-19 17:42:30 -0400

    trying to reorder

--- db/migrate/02_create_items.rb
+++ db/migrate/02_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :type
-      t.references :user, foreign_key: true
+      t.belongs_to :user, foreign_key: true
       t.string :title
       t.string :description
       t.string :image
